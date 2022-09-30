0c11fa599fa62c24aed626ac28f60d50d69a67dd
nidhish <nidhishp07@gmail.com>
2022-09-30 18:22:42 +0530

    Added spring security

--- src/main/java/com/nidhish/surveyapi/surveyapi/user/UserDetailsRestRspository.java
+++ src/main/java/com/nidhish/surveyapi/surveyapi/user/UserDetailsRestRspository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path="/")
+//@RepositoryRestResource(path="/")
 public interface UserDetailsRestRspository extends PagingAndSortingRepository<UserDetails, Long> {
 	List<UserDetails> findByRole(String role);
 }
